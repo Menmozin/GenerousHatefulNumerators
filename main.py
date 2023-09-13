c5472bfb2b97796cefa50139c3ca1d4f5852c2b7
F3ruxus <140278358+F3ruxus@users.noreply.github.com>
2023-09-13 19:47:20 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+# Juan Garcia and Mia Juarez
+inpu1 = input("What is your name? ")
+sales = input("How many sales have been made? ")
+sales_commish = float(sales) * 13 /100
+
+print(sales_commish)
+
+print(f"{inpu1} is entitiled to {sales_commish} commissions! ")
+input = input("do you like david tennant?")
+print("i love david tennant")
